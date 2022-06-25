a67639b5d3fa113b002fee5569ac3a060af8c5d8
1193131227@qq.com <1193131227@qq.com>
2022-06-25 14:15:43 +0800

    commit

--- demo02/base07.go
+++ demo02/base07.go
@@ -16,30 +16,21 @@ func functionName(input1 int, input2 int) (return1 int, return2 int) {
 //
 //Go 语言默认是值传递。
 func funTest() {
-
 }
-
 func MD5(str string) string {
 	hash := md5.New()
 	hash.Write([]byte(str))
 	return hex.EncodeToString(hash.Sum(nil))
 }
-
 func getTimestr() string {
 	return time.Now().Format("YYYY-MM-dd HH:mm:ss")
 }
-
 func getTimeUnix() int64 {
 	return time.Now().Unix()
 }
-
 func main() {
-
 	str := "12345"
-
 	fmt.Printf("str %s md5 is %s\n", str, MD5(str))
-
 	fmt.Println(getTimestr())
-
 	fmt.Println(getTimeUnix())
 }
